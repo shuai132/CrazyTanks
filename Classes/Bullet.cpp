ad1c5c1c0306c3b1cb8e22005c96be5fa56346c1
liushuai <770722922@qq.com>
2020-05-12 05:19:57 +0800

    feature: 修改子弹属性

--- Classes/Bullet.cpp
+++ Classes/Bullet.cpp
@@ -5,23 +5,25 @@ USING_NS_CC;
 Bullet::Bullet(Bullet::Type type) {
     autorelease();
 
-    _实体 = [&]{
-        if (type == Bullet::Type::Normal) {
-            Speed = 200;
-            MaxBounceCount = 5;
+    _实体 = [&] {
+        if (type == Bullet::Type::Rock2) {
+            Speed = 300;
+            Harm = 20;
+            MaxBounceCount = 2;
+            return Sprite::create("子弹/1.png");
+        } else if (type == Bullet::Type::Rock1) {
+            Speed = 300;
+            Harm = 10;
+            MaxBounceCount = 0;
+            return Sprite::create("子弹/2.png");
+        } else {
+            Speed = 260;
+            Harm = 1;
+            MaxBounceCount = 0;
 
             char buf[20];
             sprintf(buf, "子弹/0_%d.png", random(0, 3));
             return Sprite::create(buf);
-        } else {
-            Speed = 300;
-            MaxBounceCount = 10;
-
-            std::map<Bullet::Type, std::string> map = {
-                    {Bullet::Type::Rock1, "子弹/1.png"},
-                    {Bullet::Type::Rock2, "子弹/2.png"},
-            };
-            return Sprite::create(map.at(type));
         }
     }();
 
